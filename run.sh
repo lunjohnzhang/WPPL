d410411657ba08966fbf0b449841d2350b0c2378
He Jiang <srevir@foxmail.com>
2023-08-13 20:28:48 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 cd build && make -j32 && cd ..
 
 # export OMP_NUM_THREADS=128
-./build/lifelong --inputFile example_problems/random.domain/random_20.json -o test.json --simulationTime 100 --planTimeLimit 1
+./build/lifelong --inputFile example_problems/random.domain/random_20.json -o test.json --simulationTime 10 --planTimeLimit 1
 
-# ./build/lifelong --inputFile example_problems/warehouse.domain/warehouse_large_200.json -o test.json --simulationTime 100
-# ./build/lifelong --inputFile example_problems/warehouse.domain/sortation_large_400.json -o test.json --simulationTime 100 --planTimeLimit 1 --planning_window 10
+# ./build/lifelong --inputFile example_problems/warehouse.domain/warehouse_small_200.json -o test.json --simulationTime 10 --planTimeLimit 1 --planning_window 10
+# ./build/lifelong --inputFile example_problems/warehouse.domain/warehouse_large_200.json -o test.json --simulationTime 10 --planTimeLimit 1 --planning_window 10
+# ./build/lifelong --inputFile example_problems/warehouse.domain/sortation_large_2000.json -o test.json --simulationTime 100 --planTimeLimit 1 --planning_window 5
 # ./build/lifelong --inputFile example_problems/game.domain/brc202d_200.json -o test.json --simulationTime 100 --planTimeLimit 1 --planning_window 1
 # ./build/lifelong --inputFile example_problems/city.domain/paris_200.json -o test.json --simulationTime 100 --planTimeLimit 1 --planning_window 1
