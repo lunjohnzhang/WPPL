e08a39ef72e9d5e89e25edc10c5bceb24b9c21ba
HeJiang <hejiangrivers0812@gmail.com>
2023-08-26 18:44:32 +0800

    update viz from official repo: fix bugs of v1.1; update viz.sh script: include paths for more maps

--- viz.sh
+++ viz.sh
@@ -1,5 +1,10 @@
 ROOT="../.."
 MAP_PATH="example_problems/random.domain/maps/random-32-32-20.map"
+MAP_PATH="example_problems/warehouse.domain/maps/warehouse_small.map"
+MAP_PATH="example_problems/warehouse.domain/maps/warehouse_large.map"
+MAP_PATH="example_problems/warehouse.domain/maps/sortation_large.map"
+MAP_PATH="example_problems/city.domain/maps/Paris_1_256.map"
+MAP_PATH="example_problems/game.domain/maps/brc202d.map"
 PLAN_PATH="test.json"
 
 cd viz/script
