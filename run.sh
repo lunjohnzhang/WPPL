1710c4b517b21f2b75048117dd3cc380a9d39c99
HeJiang <hejiangrivers0812@gmail.com>
2023-08-28 11:13:15 -0400

    update run.sh to include more examples provided by official v1.1.1 start kits

--- run.sh
+++ run.sh
@@ -16,6 +16,8 @@ ARGS="-o test.json --simulationTime 100 --planTimeLimit 1 --fileStoragePath larg
 # ./build/lifelong --inputFile example_problems/random.domain/random_100.json $ARGS
 # ./build/lifelong --inputFile example_problems/random.domain/random_200.json $ARGS
 # ./build/lifelong --inputFile example_problems/random.domain/random_400.json $ARGS
+# ./build/lifelong --inputFile example_problems/ranodm.domain/random_600.json $ARGS
+# ./build/lifelong --inputFile example_problems/ranodm.domain/random_800.json $ARGS
 
 # warehouse:warehouse_small
 # ./build/lifelong --inputFile example_problems/warehouse.domain/warehouse_small_10.json $ARGS
@@ -32,9 +34,11 @@ ARGS="-o test.json --simulationTime 100 --planTimeLimit 1 --fileStoragePath larg
 # ./build/lifelong --inputFile example_problems/warehouse.domain/warehouse_large_600.json $ARGS
 # ./build/lifelong --inputFile example_problems/warehouse.domain/warehouse_large_800.json $ARGS
 # ./build/lifelong --inputFile example_problems/warehouse.domain/warehouse_large_1000.json $ARGS
-# NA ./build/lifelong --inputFile example_problems/warehouse.domain/warehouse_large_2000.json $ARGS
-# NA ./build/lifelong --inputFile example_problems/warehouse.domain/warehouse_large_4000.json $ARGS
-
+# ./build/lifelong --inputFile example_problems/warehouse.domain/warehouse_large_2000.json $ARGS
+# ./build/lifelong --inputFile example_problems/warehouse.domain/warehouse_large_3000.json $ARGS
+# ./build/lifelong --inputFile example_problems/warehouse.domain/warehouse_large_4000.json $ARGS
+# ./build/lifelong --inputFile example_problems/warehouse.domain/warehouse_large_5000.json $ARGS
+# ./build/lifelong --inputFile example_problems/warehouse.domain/warehouse_large_8000.json $ARGS
 
 # warehouse:sortation
 # ./build/lifelong --inputFile example_problems/warehouse.domain/sortation_large_400.json $ARGS
@@ -44,15 +48,28 @@ ARGS="-o test.json --simulationTime 100 --planTimeLimit 1 --fileStoragePath larg
 # ./build/lifelong --inputFile example_problems/warehouse.domain/sortation_large_1600.json $ARGS
 # ./build/lifelong --inputFile example_problems/warehouse.domain/sortation_large_2000.json $ARGS
 # ./build/lifelong --inputFile example_problems/warehouse.domain/sortation_large_2400.json $ARGS
+# ./build/lifelong --inputFile example_problems/warehouse.domain/sortation_large_3000.json $ARGS
+# ./build/lifelong --inputFile example_problems/warehouse.domain/sortation_large_5000.json $ARGS
+# ./build/lifelong --inputFile example_problems/warehouse.domain/sortation_large_8000.json $ARGS
 
 # game:brc202_d
 # ./build/lifelong --inputFile example_problems/game.domain/brc202d_200.json $ARGS
 # ./build/lifelong --inputFile example_problems/game.domain/brc202d_300.json $ARGS
 # ./build/lifelong --inputFile example_problems/game.domain/brc202d_400.json $ARGS 
 # ./build/lifelong --inputFile example_problems/game.domain/brc202d_500.json $ARGS 
+# ./build/lifelong --inputFile example_problems/game.domain/brc202d_1000.json $ARGS 
+# ./build/lifelong --inputFile example_problems/game.domain/brc202d_2000.json $ARGS 
+# ./build/lifelong --inputFile example_problems/game.domain/brc202d_3000.json $ARGS 
+# ./build/lifelong --inputFile example_problems/game.domain/brc202d_5000.json $ARGS 
+# ./build/lifelong --inputFile example_problems/game.domain/brc202d_8000.json $ARGS 
 
 # city:paris
 # ./build/lifelong --inputFile example_problems/city.domain/paris_200.json $ARGS
 # ./build/lifelong --inputFile example_problems/city.domain/paris_300.json $ARGS
 # ./build/lifelong --inputFile example_problems/city.domain/paris_400.json $ARGS
 # ./build/lifelong --inputFile example_problems/city.domain/paris_500.json $ARGS
+# ./build/lifelong --inputFile example_problems/city.domain/paris_1000.json $ARGS
+# ./build/lifelong --inputFile example_problems/city.domain/paris_2000.json $ARGS
+# ./build/lifelong --inputFile example_problems/city.domain/paris_3000.json $ARGS
+# ./build/lifelong --inputFile example_problems/city.domain/paris_5000.json $ARGS
+# ./build/lifelong --inputFile example_problems/city.domain/paris_8000.json $ARGS
