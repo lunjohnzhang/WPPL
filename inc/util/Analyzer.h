74926fb20105965b84561d265f6b45175fca1c7e
He Jiang <srevir@foxmail.com>
2023-08-31 21:03:35 +0800

    all config settings are stored as details in analysis

--- inc/util/Analyzer.h
+++ inc/util/Analyzer.h
@@ -6,6 +6,7 @@
 #include <fstream>
 #include "util/Dev.h"
 #include <ctime>
+#include <algorithm>
 
 class Analyzer {
 public:
@@ -36,6 +37,11 @@ public:
         data["single_agent_solver"]=config.at("single_agent_solver");
         data["planning_window"]=config.at("planning_window");
         data["simulation_window"]=config.at("simulation_window");
+        string s=config.dump();
+        std::replace(s.begin(),s.end(),',','|');
+        // TODO(hj): why not work?
+        // std::replace(s.begin(),s.end(),'\\',' ');
+        data["details"]=s;
         // TODO
     }
 
@@ -72,7 +78,8 @@ public:
             {"simulation_window","%d"},
             {"finished_tasks","%d"},
             {"timesteps","%d"},
-            {"moving_steps","%d"}
+            {"moving_steps","%d"},
+            {"details","%s"}
         };
 
 
