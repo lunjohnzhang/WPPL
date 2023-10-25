8b6783572a6a8d2b1921c61e08ae105707a8c7dc
He Jiang <srevir@foxmail.com>
2023-10-25 12:13:32 +0800

    try to use random weight 001

--- scripts/gen_weight_random.py
+++ scripts/gen_weight_random.py
@@ -51,4 +51,53 @@ def gen_weights_paris_001():
                         f.write(",")
         f.write("]")        
 
-gen_weights_paris_001()
+def gen_weights_paris_002():
+    weights=np.ones((32,32,5),dtype=np.int32)*2
+
+    c=1
+    
+    #TODO(rivers): we need to add some consistent checking for weight assignment...
+    #TODO(rivers): we need a tool to visualize weight map...
+    
+    # TODO(rivers): probably need better, make boarder and cental consistent
+    ### boarder ###
+    
+    #### top ####
+    for col in range(0,32,1): 
+        # any
+        if col%2==1:
+            # go south
+            weights[:,col,1]=2-c
+            # go north
+            weights[:,col,3]=10
+        elif col%2==0:
+            # go south
+            weights[:,col,1]=10
+            # go north
+            weights[:,col,3]=2-c
+    
+    #### left ####
+    for row in range(0,32,1):
+        # any
+        if row%2==1:
+            # go east
+            weights[row,:,0]=2-c
+            # go west
+            weights[row,:,2]=10
+        elif row%2==0:
+            # go east
+            weights[row,:,0]=10
+            # go west
+            weights[row,:,2]=2-c
+            
+    with open("random_weight_002.weight","w") as f:
+        f.write("[")
+        for row in range(32):
+            for col in range(32):
+                for dir in range(5):
+                    f.write(str(weights[row,col,dir]))
+                    if (row,col,dir)!=(31,31,4):
+                        f.write(",")
+        f.write("]")        
+        
+gen_weights_paris_002()
