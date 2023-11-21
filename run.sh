58aaaf380d3ddacffc52ebc5590620c975145640
He Jiang <srevir@foxmail.com>
2023-11-21 14:56:53 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -9,17 +9,17 @@ fi
 cd build && make -j16 lifelong && cd ..
 
 # export OMP_NUM_THREADS=1
-ARGS="-o test.json --simulationTime 1000 --planTimeLimit 1 --fileStoragePath large_files/"
+ARGS="--planTimeLimit 1 --fileStoragePath large_files/"
 
 # random:random
 # ./build/lifelong --inputFile example_problems/random.domain/random_20.json $ARGS
 # ./build/lifelong --inputFile example_problems/random.domain/random_50.json $ARGS
 # LOAD_FP="random_100_lns_step_500.snapshot" 
-# ./build/lifelong --inputFile example_problems/random.domain/random_100.json $ARGS
-# ./build/lifelong --inputFile example_problems/random.domain/random_200.json $ARGS
-# ./build/lifelong --inputFile example_problems/random.domain/random_400.json $ARGS
-# ./build/lifelong --inputFile example_problems/random.domain/random_600.json $ARGS
-# ./build/lifelong --inputFile example_problems/random.domain/random_800.json $ARGS
+./build/lifelong --inputFile example_problems/random.domain/random_100.json $ARGS -o offline_eval/1121/test_random_100.json --simulationTime 500 
+./build/lifelong --inputFile example_problems/random.domain/random_200.json $ARGS -o offline_eval/1121/test_random_200.json --simulationTime 500 
+./build/lifelong --inputFile example_problems/random.domain/random_400.json $ARGS -o offline_eval/1121/test_random_400.json --simulationTime 1000 
+./build/lifelong --inputFile example_problems/random.domain/random_600.json $ARGS -o offline_eval/1121/test_random_600.json --simulationTime 1000 
+./build/lifelong --inputFile example_problems/random.domain/random_800.json $ARGS -o offline_eval/1121/test_random_800.json --simulationTime 2000 
 
 # warehouse:warehouse_small
 # ./build/lifelong --inputFile example_problems/warehouse.domain/warehouse_small_10.json $ARGS
@@ -40,7 +40,7 @@ ARGS="-o test.json --simulationTime 1000 --planTimeLimit 1 --fileStoragePath lar
 # ./build/lifelong --inputFile example_problems/warehouse.domain/warehouse_large_3000.json $ARGS
 # ./build/lifelong --inputFile example_problems/warehouse.domain/warehouse_large_4000.json $ARGS
 # ./build/lifelong --inputFile example_problems/warehouse.domain/warehouse_large_5000.json $ARGS
-./build/lifelong --inputFile example_problems/warehouse.domain/warehouse_large_8000.json $ARGS
+./build/lifelong --inputFile example_problems/warehouse.domain/warehouse_large_8000.json $ARGS -o offline_eval/1121/test_warehouse.json --simulationTime 5000 
 
 # warehouse:sortation
 # ./build/lifelong --inputFile example_problems/warehouse.domain/sortation_large_400.json $ARGS
@@ -53,6 +53,7 @@ ARGS="-o test.json --simulationTime 1000 --planTimeLimit 1 --fileStoragePath lar
 # ./build/lifelong --inputFile example_problems/warehouse.domain/sortation_large_3000.json $ARGS
 # ./build/lifelong --inputFile example_problems/warehouse.domain/sortation_large_5000.json $ARGS
 # ./build/lifelong --inputFile example_problems/warehouse.domain/sortation_large_8000.json $ARGS
+./build/lifelong --inputFile example_problems/warehouse.domain/sortation_large_10000.json $ARGS -o offline_eval/1121/test_sortation.json --simulationTime 5000 
 
 # game:brc202_d
 # ./build/lifelong --inputFile example_problems/game.domain/brc202d_200.json $ARGS
@@ -62,6 +63,7 @@ ARGS="-o test.json --simulationTime 1000 --planTimeLimit 1 --fileStoragePath lar
 # ./build/lifelong --inputFile example_problems/game.domain/brc202d_1000.json $ARGS 
 # ./build/lifelong --inputFile example_problems/game.domain/brc202d_2000.json $ARGS 
 # ./build/lifelong --inputFile example_problems/game.domain/brc202d_3000.json $ARGS 
+./build/lifelong --inputFile example_problems/game.domain/brc202d_4000.json $ARGS  -o offline_eval/1121/test_brc202d.json --simulationTime 5000 
 # ./build/lifelong --inputFile example_problems/game.domain/brc202d_5000.json $ARGS 
 # ./build/lifelong --inputFile example_problems/game.domain/brc202d_8000.json $ARGS 
 
@@ -70,8 +72,8 @@ ARGS="-o test.json --simulationTime 1000 --planTimeLimit 1 --fileStoragePath lar
 # ./build/lifelong --inputFile example_problems/city.domain/paris_300.json $ARGS
 # ./build/lifelong --inputFile example_problems/city.domain/paris_400.json $ARGS
 # ./build/lifelong --inputFile example_problems/city.domain/paris_500.json $ARGS
-# ./build/lifelong --inputFile example_problems/city.domain/paris_1000.json $ARGS
+./build/lifelong --inputFile example_problems/city.domain/paris_1000.json $ARGS -o offline_eval/1121/test_paris_1000.json --simulationTime 2000 
 # ./build/lifelong --inputFile example_problems/city.domain/paris_2000.json $ARGS
-# ./build/lifelong --inputFile example_problems/city.domain/paris_3000.json $ARGS
+./build/lifelong --inputFile example_problems/city.domain/paris_3000.json $ARGS -o offline_eval/1121/test_paris_3000.json --simulationTime 4000 
 # ./build/lifelong --inputFile example_problems/city.domain/paris_5000.json $ARGS
 # ./build/lifelong --inputFile example_problems/city.domain/paris_8000.json $ARGS
