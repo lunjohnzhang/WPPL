a32842b910590db749563a209557231ae3d6f713
He Jiang <srevir@foxmail.com>
2023-11-25 14:04:26 +0800

    add random w036 for random

--- scripts/gen_weight_random.py
+++ scripts/gen_weight_random.py
@@ -857,6 +857,295 @@ def gen_weights_paris_034():
                         f.write(",")
         f.write("]")     
 
+def gen_weights_paris_035():
+    weights=np.ones((32,32,5),dtype=float)*2
 
+    c=-1
+    
+    #TODO(rivers): we need to add some consistent checking for weight assignment...
+    #TODO(rivers): we need a tool to visualize weight map...
+    
+    # TODO(rivers): probably need better, make boarder and cental consistent
+    ### boarder ###
+    
+    #### top ####
+    for col in range(0,32,1): 
+        # any
+        if col%2==1:
+            # go south
+            weights[:,col,1]=2-c
+            # go north
+            weights[:,col,3]=2+c
+        elif col%2==0:
+            # go south
+            weights[:,col,1]=2+c
+            # go north
+            weights[:,col,3]=2-c
+    
+    #### left ####
+    for row in range(0,32,1):
+        # any
+        if row%2==1:
+            # go east
+            weights[row,:,0]=2-c
+            # go west
+            weights[row,:,2]=2+c
+        elif row%2==0:
+            # go east
+            weights[row,:,0]=2+c
+            # go west
+            weights[row,:,2]=2-c
+    
+    for row in range(15,32):
+        for col in range(10,20):
+            weights[row,col,:]+=0.1
+            
+    for col in range(6):
+        weights[:,col,:]-=0.1
+    
+    for row in range(6):
+        weights[row,:,:]-=0.1
+        
+    for col in range(5):
+        weights[:,col,:]-=0.2
+    
+    for row in range(5):
+        weights[row,:,:]-=0.2
 
-gen_weights_paris_034()
+    for row in range(27,32):
+        for col in range(5,13):
+            weights[row,col,:]-=0.2
+            
+    for row in range(5,20):
+        for col in range(26,32):
+            weights[row,col,:]-=0.2
+            
+    for row in range(0,32):
+        for col in range(0,4):
+            weights[row,col,:]-=0.1
+    
+    for row in range(29,32):
+        for col in range(0,17):
+            weights[row,col,:]-=0.1
+    
+    # for row in range(0,3):
+    #     for col in range(23,32):
+    #         weights[row,col,:]-=0.1
+            
+    # for row in range(3,10):
+    #     for col in range(28,32):
+    #         weights[row,col,:]-=0.1
+
+    with open("random_weight_035.weight","w") as f:
+        f.write("[")
+        for row in range(32):
+            for col in range(32):
+                for dir in range(5):
+                    f.write(str(weights[row,col,dir]))
+                    if (row,col,dir)!=(31,31,4):
+                        f.write(",")
+        f.write("]")      
+
+def gen_weights_paris_036():
+    weights=np.ones((32,32,5),dtype=float)*2
+
+    c=-1
+    
+    #TODO(rivers): we need to add some consistent checking for weight assignment...
+    #TODO(rivers): we need a tool to visualize weight map...
+    
+    # TODO(rivers): probably need better, make boarder and cental consistent
+    ### boarder ###
+    
+    #### top ####
+    for col in range(0,32,1): 
+        # any
+        if col%2==1:
+            # go south
+            weights[:,col,1]=2-c
+            # go north
+            weights[:,col,3]=2+c
+        elif col%2==0:
+            # go south
+            weights[:,col,1]=2+c
+            # go north
+            weights[:,col,3]=2-c
+    
+    #### left ####
+    for row in range(0,32,1):
+        # any
+        if row%2==1:
+            # go east
+            weights[row,:,0]=2-c
+            # go west
+            weights[row,:,2]=2+c
+        elif row%2==0:
+            # go east
+            weights[row,:,0]=2+c
+            # go west
+            weights[row,:,2]=2-c
+    
+    for row in range(15,32):
+        for col in range(10,20):
+            weights[row,col,:]+=0.1
+            
+    for col in range(6):
+        weights[:,col,:]-=0.1
+    
+    for row in range(6):
+        weights[row,:,:]-=0.1
+        
+    for col in range(5):
+        weights[:,col,:]-=0.2
+    
+    for row in range(5):
+        weights[row,:,:]-=0.2
+
+    for row in range(27,32):
+        for col in range(5,13):
+            weights[row,col,:]-=0.2
+            
+    for row in range(5,20):
+        for col in range(26,32):
+            weights[row,col,:]-=0.2
+            
+    for row in range(0,32):
+        for col in range(0,4):
+            weights[row,col,:]-=0.1
+    
+    for row in range(29,32):
+        for col in range(0,17):
+            weights[row,col,:]-=0.1
+    
+    # for row in range(0,3):
+    #     for col in range(23,32):
+    #         weights[row,col,:]-=0.1
+            
+    # for row in range(3,10):
+    #     for col in range(28,32):
+    #         weights[row,col,:]-=0.1
+    
+    for row in range(6,21):
+        for col in range(7,13):
+            weights[row,col,:]-=0.1
+            
+    for row in range(6,10):
+        for col in range(13,23):
+            weights[row,col,:]-=0.1
+
+    with open("random_weight_036.weight","w") as f:
+        f.write("[")
+        for row in range(32):
+            for col in range(32):
+                for dir in range(5):
+                    f.write(str(weights[row,col,dir]))
+                    if (row,col,dir)!=(31,31,4):
+                        f.write(",")
+        f.write("]")      
+
+def gen_weights_paris_037():
+    weights=np.ones((32,32,5),dtype=float)*2
+
+    c=-1
+    
+    #TODO(rivers): we need to add some consistent checking for weight assignment...
+    #TODO(rivers): we need a tool to visualize weight map...
+    
+    # TODO(rivers): probably need better, make boarder and cental consistent
+    ### boarder ###
+    
+    #### top ####
+    for col in range(0,32,1): 
+        # any
+        if col%2==1:
+            # go south
+            weights[:,col,1]=2-c
+            # go north
+            weights[:,col,3]=2+c
+        elif col%2==0:
+            # go south
+            weights[:,col,1]=2+c
+            # go north
+            weights[:,col,3]=2-c
+    
+    #### left ####
+    for row in range(0,32,1):
+        # any
+        if row%2==1:
+            # go east
+            weights[row,:,0]=2-c
+            # go west
+            weights[row,:,2]=2+c
+        elif row%2==0:
+            # go east
+            weights[row,:,0]=2+c
+            # go west
+            weights[row,:,2]=2-c
+    
+    for row in range(15,32):
+        for col in range(10,20):
+            weights[row,col,:]+=0.1
+            
+    for col in range(6):
+        weights[:,col,:]-=0.1
+    
+    for row in range(6):
+        weights[row,:,:]-=0.1
+        
+    for col in range(5):
+        weights[:,col,:]-=0.2
+    
+    for row in range(5):
+        weights[row,:,:]-=0.2
+
+    for row in range(27,32):
+        for col in range(5,13):
+            weights[row,col,:]-=0.2
+            
+    for row in range(5,20):
+        for col in range(26,32):
+            weights[row,col,:]-=0.2
+            
+    for row in range(0,32):
+        for col in range(0,4):
+            weights[row,col,:]-=0.1
+    
+    for row in range(29,32):
+        for col in range(0,17):
+            weights[row,col,:]-=0.1
+    
+    # for row in range(0,3):
+    #     for col in range(23,32):
+    #         weights[row,col,:]-=0.1
+            
+    # for row in range(3,10):
+    #     for col in range(28,32):
+    #         weights[row,col,:]-=0.1
+    
+    for row in range(6,21):
+        for col in range(7,13):
+            weights[row,col,:]-=0.1
+            
+    for row in range(6,10):
+        for col in range(13,23):
+            weights[row,col,:]-=0.1
+            
+    for row in range(6,16):
+        for col in range(22,32):
+            weights[row,col,:]-=0.01
+
+    for row in range(21,26):
+        for col in range(10,16):
+            weights[row,col,:]-=0.01
+
+    with open("random_weight_037.weight","w") as f:
+        f.write("[")
+        for row in range(32):
+            for col in range(32):
+                for dir in range(5):
+                    f.write(str(weights[row,col,dir]))
+                    if (row,col,dir)!=(31,31,4):
+                        f.write(",")
+        f.write("]")     
+
+gen_weights_paris_037()
