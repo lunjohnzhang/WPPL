4b01f1b3392016339beb4a01f875ae7582222fa3
He Jiang <srevir@foxmail.com>
2023-12-10 11:57:55 +0800

    organize run_py_driver.py code

--- run_py_driver.py
+++ run_py_driver.py
@@ -5,7 +5,6 @@ from map import Map
 
 map_path="example_problems/random.domain/maps/random-32-32-20.map"
 full_weight_path="scripts/random_weight_050.w"
-config_path="py_configs/random-32-32-20_600.json"
 with_wait_costs=True
 
 map=Map(map_path)
@@ -14,41 +13,6 @@ map.print_graph(map.graph)
 import json
 with open(full_weight_path) as f:
     full_weights=json.load(f)
-
-# def compress_weights(map,full_weights):
-#     compressed_weights=[]
-    
-#     compressed_weights.append(full_weights[4]) # wait
-    
-#     for y in range(map.height):
-#         for x in range(map.width):
-#             weight_idx=y*map.width+x
-            
-#             if map.graph[y,x]==1:
-#                 continue
-            
-#             if (x+1)<map.width and map.graph[y,x+1]==0:
-#                 compressed_weights.append(full_weights[5*weight_idx+0])
-                
-#             if (y-1)>=0 and map.graph[y-1,x]==0:
-#                 compressed_weights.append(full_weights[5*weight_idx+3])
-                
-#             if (x-1)>=0 and map.graph[y,x-1]==0:
-#                 compressed_weights.append(full_weights[5*weight_idx+2])
-            
-#             if (y+1)<map.height and map.graph[y+1,x]==0:
-#                 compressed_weights.append(full_weights[5*weight_idx+1])
-                
-#             if (weight_idx==0):
-#                 print(compressed_weights)
-                
-#     print("size",compressed_weights.__len__())
-
-#     compressed_weights="["+",".join([str(w) for w in compressed_weights])+"]"
-#     return compressed_weights
-
-# compressed_weights_json_str=compress_weights(map,full_weights,with_wait_costs)
-
 
 def compress_weights_with_wait_costs(map,full_weights):
     compressed_wait_costs=[]
@@ -105,30 +69,17 @@ print(compressed_weights_json_str)
 import py_driver
 print(py_driver.playground())
 
-# import os
-# how do we specify OMP_NUM_THREADS=1, may be directly set in environ?
-# os.environ["OMP_NUM_THREADS"] = "1"
-
-# EXECUTABLE="./build/lifelong_comp"
-# INPUT_FILE="example_problems/random.domain/random_600.json" # random_xxx means random map with xxx agents. we care about 100,200,400,600,800.
-# OUTPUT_FILE="test_py_driver.json" # 
-# SIMULATION_TIME=1000 # simulate how many steps # for random_100,200,400,600,800, we use 500,500,1000,1000,1000 steps accordingly.
-# PLAN_TIME_LIMIT=1 # how many seconds to plan for each step, no need to change
-# FILE_STORAGE_PATH="large_files/" # where to store the precomputed large files, no need (to change) in the weight opt case.
-
-# cmd=f"{EXECUTABLE} --inputFile {INPUT_FILE} -o {OUTPUT_FILE} --simulationTime {SIMULATION_TIME} --planTimeLimit {PLAN_TIME_LIMIT} --fileStoragePath {FILE_STORAGE_PATH}"
-
-# the cmd is the string command above, which is the same command to run ./build/lifelong in the terminal.
-# the compress_weights_json_str is a weight represented as [w0,w1,w2,....] as required by weight opt program.
-# the return values now include:
-# 1. throughput double
-# 2. vertexUsage 1-d double json array, N_v
-# 3. edgeUsage  2-d double json array, N_v*N_v
 import json
+# py_configs contains the best configs used for random-32-32-20 map with 100,200,400,600,800 agents.
+config_path="py_configs/random-32-32-20_600.json"
 with open(config_path) as f:
     config=json.load(f)
     config_str=json.dumps(config)
 
+# the return values now include:
+# 1. throughput double
+# 2. vertexUsage 1-d double json array, N_v
+# 3. edgeUsage  2-d double json array, N_v*N_v
 ret=py_driver.run(
     map_path="example_problems/random.domain/maps/random-32-32-20.map",
     simulation_steps=1000,
@@ -139,8 +90,8 @@ ret=py_driver.run(
     num_tasks=100000,
     seed=0,
     # else we need specify agents and tasks path to load data.
-    agents_path="example_problems/random.domain/agents/random_600.agents",
-    tasks_path="example_problems/random.domain/tasks/random-32-32-20-600.tasks",
+    # agents_path="example_problems/random.domain/agents/random_600.agents",
+    # tasks_path="example_problems/random.domain/tasks/random-32-32-20-600.tasks",
     # weights are the edge weights, wait_costs are the vertex wait costs
     # if not specified here, then the program will use the one specified in the config file.
     weights=compressed_weights_json_str,
@@ -164,7 +115,6 @@ print(np.array(analysis["tile_usage"]).shape)
 print(np.array(analysis["edge_pair_usage"]).shape)
 
 print(analysis["throughput"],analysis["edge_pair_usage_mean"],analysis["edge_pair_usage_std"])
-
 
 ##### Only use the following for weight opt case #####
 # because the order of orientation is different in competition code and weight opt code.
