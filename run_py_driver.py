acb5439599657f7f6551a3479dd235047a76cf7e
Rivers <rivers@foxmail.com>
2024-01-19 18:08:58 -0500

    clean run_py_driver script

--- run_py_driver.py
+++ run_py_driver.py
@@ -3,85 +3,24 @@ sys.path.append('build')
 sys.path.append('scripts')
 from map import Map
 
-map_path="example_problems/warehouse.domain/maps/kiva_large_w_mode.map"
+map_path="example_problems/warehouse.domain/maps/kiva_69x69.map"
 # full_weight_path="scripts/random_weight_001.w"
 with_wait_costs=True
 
 map=Map(map_path)
 map.print_graph(map.graph)
 
-# import json
-# with open(full_weight_path) as f:
-#     full_weights=json.load(f)
-
-# def compress_weights_with_wait_costs(map,full_weights):
-#     compressed_wait_costs=[]
-#     for y in range(map.height):
-#         for x in range(map.width):
-#             weight_idx=y*map.width+x
-            
-#             if map.graph[y,x]==1:
-#                 continue
-            
-#             compressed_wait_costs.append(full_weights[5*weight_idx+4])
-            
-#     print("wait costs size",compressed_wait_costs.__len__())
-
-#     compressed_wait_costs="["+",".join([str(w) for w in compressed_wait_costs])+"]"
-    
-    
-#     compressed_weights=[]
-    
-#     for y in range(map.height):
-#         for x in range(map.width):
-#             weight_idx=y*map.width+x
-            
-#             if map.graph[y,x]==1:
-#                 continue
-            
-#             if (x+1)<map.width and map.graph[y,x+1]==0:
-#                 compressed_weights.append(full_weights[5*weight_idx+0])
-                
-#             if (y-1)>=0 and map.graph[y-1,x]==0:
-#                 compressed_weights.append(full_weights[5*weight_idx+3])
-                
-#             if (x-1)>=0 and map.graph[y,x-1]==0:
-#                 compressed_weights.append(full_weights[5*weight_idx+2])
-            
-#             if (y+1)<map.height and map.graph[y+1,x]==0:
-#                 compressed_weights.append(full_weights[5*weight_idx+1])
-                
-#             if (weight_idx==0):
-#                 print(compressed_weights)
-                
-#     print("edge weights size",compressed_weights.__len__())
-
-#     compressed_weights="["+",".join([str(w) for w in compressed_weights])+"]"
-#     return compressed_wait_costs, compressed_weights
-
-
-# compressed_wait_costs_json_str, compressed_weights_json_str=compress_weights_with_wait_costs(map,full_weights)
-
-# print(compressed_wait_costs_json_str)
-# print(compressed_weights_json_str)
-
-
 import py_driver
 print(py_driver.playground())
 
 import json
-# py_configs contains the best configs used for random-32-32-20 map with 100,200,400,600,800 agents.
 config_path="configs/pibt_default_no_rot.json"
 with open(config_path) as f:
     config=json.load(f)
     config_str=json.dumps(config)
 
-# the return values now include:
-# 1. throughput double
-# 2. vertexUsage 1-d double json array, N_v
-# 3. edgeUsage  2-d double json array, N_v*N_v
 ret=py_driver.run(
-    map_path="example_problems/warehouse.domain/maps/kiva_69x69.map",
+    map_path=map_path,
     simulation_steps=1000,
     # for the problem instance we use:
     # if random then we need specify the number of agents and total tasks, also random seed,
