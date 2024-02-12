b9e027dfcbcfc320b117d1b6fd9f9ee9c0c97a25
Yulun Zhang <yulunzha@usc.edu>
2024-02-12 15:55:37 -0500

    remove comment

--- src/LNS/Parallel/GlobalManager.cpp
+++ src/LNS/Parallel/GlobalManager.cpp
@@ -402,12 +402,12 @@ bool GlobalManager::_run_async(TimeLimiter & time_limiter) {
         average_group_size /= (double)(iteration_stats.size() - 1);
 
     // elapse=time_limiter.get_elapse();
-    cout << getSolverName() << ": "
-        << "runtime = " << elapse << ", "
-        << "iterations = " << iteration_stats.size()-1 << ", "
-        << "solution cost = " << sum_of_costs << ", "
-        << "initial solution cost = " << initial_sum_of_costs << ", "
-        << "failed iterations = " << num_of_failures << endl;
+    // cout << getSolverName() << ": "
+    //     << "runtime = " << elapse << ", "
+    //     << "iterations = " << iteration_stats.size()-1 << ", "
+    //     << "solution cost = " << sum_of_costs << ", "
+    //     << "initial solution cost = " << initial_sum_of_costs << ", "
+    //     << "failed iterations = " << num_of_failures << endl;
 
     return true;
 }
