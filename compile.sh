f50f5d7d0a2d0d007ddfe68f16004b2d3ea2fd94
zanghz21 <zanghzhi@gmail.com>
2024-07-10 18:16:21 -0400

    further del net

--- compile.sh
+++ compile.sh
@@ -5,8 +5,7 @@ mkdir build
 # build exec for cpp
 
 cd build
-cmake ../ -DEIGEN3_INCLUDE_DIR=/media/project0/hongzhi/TrafficFlowMAPF/third_party/eigen \
-    -DMINIDNN_DIR=/media/project0/hongzhi/TrafficFlowMAPF/third_party/MiniDNN/include
+cmake ../ 
 make -j
 
 
